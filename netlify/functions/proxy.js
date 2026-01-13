a8eab75d6d6405fe14939a8b5d8001b5b4374ef4
maksim-3d <rogov.maksim2001@gmail.com>
2026-01-13 22:46:47 +0300

    Create proxy.js

---
+++ netlify/functions/proxy.js
@@ -0,0 +1,20 @@
+exports.handler = async function(event, context) {
+  try {
+    const response = await fetch('http://78.40.188.120:3000/');
+    const data = await response.json();
+    
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
